c1d86d1da7dbc2982773a6419dccb276d1f2a1e0
Rebel1745 <31040719+Rebel1745@users.noreply.github.com>
2023-06-01 17:31:36 +0100

    Hop animation
    Slightly buggy animation hopping from square to square

--- Snakes And Ladders/Assets/Scripts/PlayerPiece.cs
+++ Snakes And Ladders/Assets/Scripts/PlayerPiece.cs
@@ -7,26 +7,72 @@ public class PlayerPiece : MonoBehaviour
     [SerializeField] Tile startingTile;
     Tile currentTile;
 
-    // Start is called before the first frame update
+    // movement variables
+    Tile[] moveQueue;
+    int moveQueueIndex;
+    Vector3 targetPosition;
+    Vector3 velocity = Vector3.zero;
+    [SerializeField] float smoothTime = 0.25f;
+    [SerializeField] float smoothDistance = 0.01f;
+    [SerializeField] float maxHeight = 0.5f;
+    [SerializeField] AnimationCurve heightCurve;
+    Vector3 targetPositionWithHeight;
+    float heightTime;
+    float targetHeight;
+    
     void Start()
     {
-        
+        targetPosition = this.transform.position;
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        MovePiece();
+    }
+
+    void MovePiece()
+    {
+        if(Vector3.Distance(this.transform.position, targetPosition) < smoothDistance)
+        {
+            AdvanceMoveQueue();
+        }
+
+        targetHeight = heightCurve.Evaluate(heightTime / smoothTime) * maxHeight;
+        // TODO: Why is this not working? it is giving me wrong numbers
+        print(heightTime + " / " + smoothTime + " = " + heightTime / smoothTime);
+        targetPositionWithHeight = new Vector3(targetPosition.x, targetHeight , targetPosition.z);
+
+        this.transform.position = Vector3.SmoothDamp(this.transform.position, targetPositionWithHeight, ref velocity, smoothTime);
+
+        heightTime += Time.deltaTime;
+    }
+
+    void AdvanceMoveQueue()
+    {
+        if (moveQueue != null && moveQueueIndex < moveQueue.Length)
+        {
+            SetNewTargetPosition(moveQueue[moveQueueIndex].transform.position);
+            moveQueueIndex++;
+        }
+    }
+
+    void SetNewTargetPosition(Vector3 pos)
+    {
+        targetPosition = pos;
+        velocity = Vector3.zero;
+        heightTime = 0f;
     }
 
     private void OnMouseUp()
     {
-        print("Clicked");
         if (!DiceRoller.instance.IsDoneRolling)
             return;
 
         // move this piece
         int spacesToMove = DiceRoller.instance.DiceTotal;
+
+        print("Clicked. Moving " + spacesToMove + " spaces");
+        moveQueue = new Tile[spacesToMove];
 
         Tile finalTile = currentTile;
 
@@ -40,9 +86,12 @@ public class PlayerPiece : MonoBehaviour
             {
                 finalTile = finalTile.NextTile;
             }
+
+            moveQueue[i] = finalTile;
         }
 
-        this.transform.position = finalTile.transform.position;
+        moveQueueIndex = 0;
+
         currentTile = finalTile;
     }
 }
